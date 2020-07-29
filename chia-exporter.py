8438084c7f72e5e79ab72355050ed30fd88d38a3
Mal Curtis <mal@mal.co.nz>
2020-07-29 22:51:51 +1200

    Move netspace to rpc from subprocess

--- chia-exporter.py
+++ chia-exporter.py
@@ -3,12 +3,12 @@
 Chia Exporter for Prometheus
 """
 
-from time import sleep
+import datetime
+import time
 import asyncio
 import logging
-import subprocess
 import argparse
-import re
+from time import sleep, struct_time, localtime
 
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
@@ -19,35 +19,28 @@ sys.path.append('/opt/chia-blockchain')
 
 from src.util.config import load_config
 from src.util.default_root import DEFAULT_ROOT_PATH
+from src.rpc.full_node_rpc_client import FullNodeRpcClient
 from src.rpc.harvester_rpc_client import HarvesterRpcClient
 
+def human_local_time(timestamp):
+    time_local = struct_time(localtime(timestamp))
+    return time.strftime("%a %b %d %Y %T %Z", time_local)
 
 class ChiaCollector(object):
     def __init__(self, args):
         self.args = args
+        self.config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
 
     def collect(self):
         if self.args.collect_node:
-            netspace = subprocess.run(["chia", "netspace"], stdout=subprocess.PIPE, text=True)
-            if "Connection Failure" in netspace.stdout:
-                # Connection Failure: "Connection error. Check if full node is running at None"
-                logging.info("Failed to get netspace: %s", netspace.stdout)
-                return
-            matches = re.findall(r'\d+\.\d+TiB', netspace.stdout)
-            if len(matches) < 1:
-                logging.info("Failed to get netspace match: %s", netspace.stdout)
-                return
-            tib = float(matches[0].strip('TiB'))
-            gib = tib * 1024
-            mib = gib * 1024
-            kib = mib * 1024
-            b = kib * 1024
-            logging.debug("Received: %dTiB %dB", tib, b)
+            netspace = asyncio.run(self.get_network_space())
+            logging.debug("Received: %dB", netspace)
 
             yield GaugeMetricFamily(
                 'chia_node_netspace_bytes',
                 'The Chia Netspace in Bytes',
-                value=int(b))
+                value=netspace)
+
         if self.args.collect_harvester:
             plots = asyncio.run(self.get_plots())
             gauges = {
@@ -58,18 +51,46 @@ class ChiaCollector(object):
             }
             for plot in plots['plots']:
                 gauges['bytes'].add_metric(
-                    [plot['filename'], plot['plot-seed'], plot['plot_public_key'], plot['pool_public_key'], plot['farmer_public_key'], plot['local_sk'], str(plot['size'])],
+                    [plot['filename'], str(plot['plot-seed']), str(plot['plot_public_key']), str(plot['pool_public_key']), str(plot['farmer_public_key']), str(plot['local_sk']), str(plot['size'])],
                     plot['file_size'])
             logging.debug("Got plots %s", plots)
             yield gauges['bytes']
 
     async def get_plots(self):
-        config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-        self_hostname = config["self_hostname"]
-        rpc_port = config["harvester"]["rpc_port"]
+        self_hostname = self.config["self_hostname"]
+        rpc_port = self.config["harvester"]["rpc_port"]
         harvester_client = await HarvesterRpcClient.create(self_hostname, rpc_port)
         plots = await harvester_client.get_plots()
+        harvester_client.close()
         return plots
+
+    # Stolen from src/cmds/netspace.py
+    async def get_network_space(self):
+        self_hostname = self.config["self_hostname"]
+        rpc_port = self.config["full_node"]["rpc_port"]
+        client = await FullNodeRpcClient.create(self_hostname, rpc_port)
+        blockchain_state = await client.get_blockchain_state()
+        newer_block_height = blockchain_state["lca"].data.height
+        newer_block_header = await client.get_header_by_height(newer_block_height)
+        older_block_height = newer_block_height - 24
+        older_block_header = await client.get_header_by_height(older_block_height)
+        newer_block_header_hash = str(newer_block_header.get_hash())
+        older_block_header_hash = str(older_block_header.get_hash())
+        elapsed_time = (
+            newer_block_header.data.timestamp - older_block_header.data.timestamp
+        )
+        newer_block_time_string = human_local_time(
+            newer_block_header.data.timestamp
+        )
+        older_block_time_string = human_local_time(
+            older_block_header.data.timestamp
+        )
+        time_delta = datetime.timedelta(seconds=elapsed_time)
+        network_space_bytes = await client.get_network_space(
+            newer_block_header_hash, older_block_header_hash
+        )
+        client.close()
+        return network_space_bytes
 
 def main(args):
     logging.getLogger().setLevel(20)
